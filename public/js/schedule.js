f45289086e4e63c53ce3bff6c2387bca70472d78
Joe Longstreet <joelongstreet@gmail.com>
2014-10-14 16:08:23 -0500

    add a time controller

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -1,18 +1,56 @@
 $(function(){
-  var timeStamps = nowAndLater();
 
+  // prepopulate the date picker and attache event listeners to the prev and
+  // next date selectors
+  updateDateController(new Date().getTime());
+  $('.date-controller').find('a').click(function(e){
+    e.preventDefault();
+    var selectedTime = $(this).data('time');
+    updateDateController(selectedTime);
+  });
+
+  // Fetch busy data for each user
+  var ISOStamps = nowAndLaterAsIsos();
   users.forEach(function(user){
     fetchBusyData(
-      user.emails[0].value, timeStamps.now, timeStamps.later
+      user.emails[0].value, ISOStamps.now, ISOStamps.later
     ).then(function(res){
       console.log(res);
     }).fail(function(err){
-
+      console.log(err);
     });
   });
+
 });
 
 
+// time should be passed as a UTC
+var updateDateController = function(selectedTime){
+  var $container = $('.date-controller');
+
+  var format = function(timestamp){
+    var months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+    var date = new Date(timestamp);
+    var day = date.getDate();
+    var month = date.getMonth();
+
+    return [
+      months[month],
+      ' ',
+      day
+    ].join('');
+  };
+
+  var before = selectedTime - 86400000;
+  var later = selectedTime + 86400000;
+
+  $container.find('.prev-date').text(format(before)).data('time', before);
+  $container.find('.current-date').text(format(selectedTime)).data('time', selectedTime);
+  $container.find('.next-date').text(format(later)).data('time', later);
+};
+
+
+// times should be passed as ISO strings
 var fetchBusyData = function(userName, timeMin, timeMax){
   var deferred = Q.defer();
 
@@ -36,7 +74,7 @@ var fetchBusyData = function(userName, timeMin, timeMax){
 };
 
 
-var nowAndLater = function(){
+var nowAndLaterAsIsos = function(){
   var now = new Date();
   var later = now.getTime() + 7884000000;
 
