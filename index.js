7eee5a49317f117e3e52621eb791f143c7c73ea9
Joe Longstreet <joelongstreet@gmail.com>
2014-10-14 09:47:17 -0500

    dont use root path for auth

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
 var express = require('express');
+var path = require('path');
 var config = require('./config')();
 var passport = require('./lib/auth').passport;
 var routes = require('./lib/routes');
 
 
 var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
+
 var server = require('http').Server(app);
 
 server.listen(config.PORT, function(){
   console.log('server listening on port', config.PORT);
 });
 
-app.get('/', passport.authenticate('google', {
+
+app.get('/', routes.home);
+
+app.get('/auth', passport.authenticate('google', {
   accessType: 'offline',
   approvalPrompt: 'force',
   scope: ['profile', 'email', 'https://www.googleapis.com/auth/calendar']
-}), routes.home);
+}), routes.loginError);
 
 app.get('/login-error', routes.loginError);
 
 app.get('/oauth2callback',
-  passport.authenticate('google', { failureRedirect: '/login-error'}),
+  passport.authenticate('google', { failureRedirect: '/login-error' }),
   routes.oauth
 );
 
-app.get('/partner/:identifier', routes.getNextCalendarEvent);
-app.get('/user/:identifier', routes.getNextCalendarEvent);
+app.get('/user/:email', routes.getNextCalendarEvent);
