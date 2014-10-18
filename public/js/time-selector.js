610c9311e1f9e93ac10ed8f3c1f82a3bf9159fa9
Joe Longstreet <joelongstreet@gmail.com>
2014-10-17 20:43:32 -0500

    get rid of unnecessary timeout, ensure use of properly namespaced elements

--- public/js/time-selector.js
+++ public/js/time-selector.js
@@ -2,8 +2,10 @@
 
   $.fn.timeSelector = function(opts){
     var settings = $.extend({
-      autofill : false,
-      cleanup  : false
+      autoFill      : false,
+      cleanUp       : false,
+      cleanUpTimer  : 300,
+      done          : function(){}
     }, opts);
 
 
@@ -34,50 +36,47 @@
     var handleMouseRelease = function(){
       mouseIsDown = false;
 
-      if(settings.autofill) autoFill();
-      if(settings.cleanup) cleanUp();
+      if(settings.autoFill) autoFill();
+      if(settings.cleanUp) cleanUp(settings.done);
     };
 
 
     // smart fills spaces that look like they shold be filled
     var autoFill = function(){
-      clearTimeout(autoFillTimer);
+      var $last = $('tr.last-selected').last();
+      var $next = $last.next();
+      var $prev = $last.prev();
 
-      autoFillTimer = setTimeout(function(){
-        var $last = $('tr.last-selected').last();
-        var $next = $last.next();
-        var $prev = $last.prev();
+      var goForwards = true;
+      while(goForwards){
+        if(!($next.hasClass('selected')) && $next.next().hasClass('selected'))
+          $next.addClass('selected');
+        else if(!($next.hasClass('selected')))
+          goForwards = false;
 
-        var goForwards = true;
-        while(goForwards){
-          if(!($next.hasClass('selected')) && $next.next().hasClass('selected'))
-            $next.addClass('selected');
-          else if(!($next.hasClass('selected')))
-            goForwards = false;
+        $next = $next.next();
+        if(!$next.length) goForwards = false;
+      }
 
-          $next = $next.next();
-          if(!$next.length) goForwards = false;
-        }
+      var goBackwards = true;
+      while(goBackwards){
+        if(!($prev.hasClass('selected')) && $prev.prev().hasClass('selected'))
+          $prev.addClass('selected');
+        else if(!($prev.hasClass('selected')))
+          goBackwards = false;
 
-        var goBackwards = true;
-        while(goBackwards){
-          if(!($prev.hasClass('selected')) && $prev.prev().hasClass('selected'))
-            $prev.addClass('selected');
-          else if(!($prev.hasClass('selected')))
-            goBackwards = false;
-
-          $prev = $prev.prev();
-          if(!$prev.length) goBackwards = false;
-        }
-      }, 100);
+        $prev = $prev.prev();
+        if(!$prev.length) goBackwards = false;
+      }
     };
 
 
-    var cleanUp = function(){
+    // only allows a single block to be selected
+    var cleanUp = function(next){
       clearTimeout(cleanUpTimer);
 
       cleanUpTimer = setTimeout(function(){
-        var $last = $('tr.last-selected').last();
+        var $last = $($tr.filter('.last-selected')[0]);
         var okIndices = [$last.index('tr')];
         var $next = $last.next();
         var $prev = $last.prev();
@@ -100,16 +99,17 @@
           if(!$prev.length) goBackwards = false;
         }
 
-        $('tr').each(function(i, el){
+        $tr.each(function(i, el){
           if(okIndices.indexOf(i) == -1){
             $(el).removeClass('selected');
           }
         });
-      }, 300);
+
+        next($tr.filter('.selected'));
+      }, settings.cleanUpTimer);
     };
 
 
-    var autoFillTimer;
     var cleanUpTimer;
     var mouseIsDown = false;
     var $tr = $(this).find('tr');
